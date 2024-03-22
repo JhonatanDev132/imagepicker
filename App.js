11e955c0205a575712828c225a293092291670b2
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-03-22 08:55:06 -0300

    .

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    <>
+      <StatusBar/>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Escolher foto"/>
+      <Image style={{ width: 300, height: 300 }}/>
     </View>
+    </>
   );
 }
 
